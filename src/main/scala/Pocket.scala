6c377f99f28f41531c458acbe0b69c63bfc0fa2f
Richard Dallaway <richard@dallaway.com>
2018-03-19 17:46:30 +0000

    wip on posting to add endopoint

--- src/main/scala/Pocket.scala
+++ src/main/scala/Pocket.scala
@@ -1,3 +1,29 @@
-class Pocket {
+case class PocketEntry(
+  url: String,
+  time: Long,
+  title: Option[String],
+  tags: Set[String],
+)
+
+case class Credentials(
+  consumer_key: String,
+  access_token: String
+)
+
+case class Pocket(credentials: Credentials) {
+
+  import com.softwaremill.sttp._
+  import io.circe.syntax._
+  import io.circe.generic.auto._
+
+  def add(entry: PocketEntry): Request[String, Nothing] =
+    sttp.post(uri"https://getpocket.com/v3/add").body(entry)
+
+  private lazy val creds = credentials.asJson
+
+  private implicit val entrySerializer:  BodySerializer[PocketEntry] = p => {
+    val json: String = (p.asJson deepMerge creds).spaces2
+    StringBody(json, "UTF-8", Some("application/json"))
+  }
 
 }
